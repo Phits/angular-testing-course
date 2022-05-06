88ac7f4938b4c48dd6b15b88e8128671e5bd43b6
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2022-05-06 11:36:01 -0400

    Unit testing ui components

--- angular-testing-course-1-start/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ angular-testing-course-1-start/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -7,33 +7,70 @@ import {By} from '@angular/platform-browser';
 import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
 import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
-
-
+import {componentFactoryResolverProviderDef} from "@angular/compiler/src/view_compiler/provider_compiler";
 
 
 describe('CoursesCardListComponent', () => {
 
+  let component: CoursesCardListComponent;
+  // Utility Type for testing components
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
+
+
+  beforeEach(waitForAsync(() => {
+
+    TestBed.configureTestingModule({
+        imports: [CoursesModule]
+    })
+    .compileComponents()
+      .then(() => {
+
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+
+      });
+
+  }));
 
   it("should create the component", () => {
 
-   pending();
+   expect(component).toBeTruthy();
 
   });
 
-
   it("should display the course list", () => {
 
-    pending();
+    component.courses = setupCourses();
+
+    fixture.detectChanges();
+
+    const cards = el.queryAll((By.css(".course-card")));
+
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12, 'Unexpected number of courses');
 
   });
 
 
   it("should display the first course", () => {
 
-      pending();
+    component.courses = setupCourses();
+
+    fixture.detectChanges();
+
+    const course = component.courses[0];
+
+    const card = el.query(By.css('.course-card:first-child')),
+        title = card.query(By.css('mat-card-title')),
+        image = card.query(By.css('img'));
+
+    expect(card).toBeTruthy("Could not find card");
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
 
   });
-
 
 });
 
